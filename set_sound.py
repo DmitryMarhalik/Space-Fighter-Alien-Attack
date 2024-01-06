d2e7bac78d2ce2518d8a5bfb7e5cfdc4288b3324
dm <dmitrymarhalik@gmail.com>
2024-01-07 00:56:29 +0300

    docstrings

--- set_sound.py
+++ set_sound.py
@@ -1,7 +1,14 @@
 import pygame
 
 
-def config_sound(file, volume):
+def config_sound(file: str, volume: float) -> pygame.mixer.Sound:
+    """ Настраивает звуковой файл с определенной громкостью.
+    Args:
+        file (str): Путь к звуковому файлу.
+        Volume (float): Уровень громкости звука.
+    Return:
+        pygame.mixer.Sound: настроенный звуковой объект.
+    """
     sound = pygame.mixer.Sound(file)
     sound.set_volume(volume)
     return sound
--- space_game.py
+++ space_game.py
@@ -12,29 +12,39 @@ FPS = 60  # количество кадров в секунду
 
 
 def run():
+    """
+    Запуск игрового цикла.
+    """
+    # Инициализация pygame и mixer
     pygame.mixer.pre_init(44100, -16, 1, 512)
     pygame.init()
-    # добавление звуков
+
+    # Загрузка и воспроизведение фоновой музыки
     pygame.mixer.music.load('sounds/bg_music.mp3')
     pygame.mixer.music.play(-1, 27.0, 4000)
     pygame.mixer.music.set_volume(0.2)
+
+    # Загрузка и настройка звуковых эффектов
     shoot = config_sound('sounds/shoot.mp3', 0.4)
     hit = config_sound('sounds/bang.ogg', 0.4)
     crush = config_sound('sounds/crush_spaceship.ogg', 0.4)
     next_level = config_sound('sounds/level.ogg', 0.4)
     game_over = config_sound('sounds/game_over.mp3', 0.4)
 
+    # Загрузка изображений
     icon = pygame.image.load('images/icon.png')
     bg_image = pygame.image.load('images/space.png')
     level_failed = pygame.image.load('images/level_failed.png')
     you_died = pygame.image.load('images/you_died.jpg')
 
+    # Установка свойств окна игры
     pygame.display.set_icon(icon)
     pygame.display.set_caption('Space Fighter: Alien Attack')
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     clock = pygame.time.Clock()
 
+    # Создание игровых объектов
     spaceship = SpaceShip(screen)
     rockets = Group()
     ufos = Group()
@@ -45,14 +55,21 @@ def run():
     controls.create_ufos_army(screen, ufos)
 
     while True:
+        # Обработка событий
         controls.events(screen, spaceship, rockets, shoot)
+
         if stats.play_game:
+            # Обновление положение космического корабля
             spaceship.update_position()
+
+            # Обновление НЛО и состояние игры.
             controls.update_ufos(stats, screen, spaceship, ufos, rockets, level_failed, you_died, crush, next_level,
                                  msg_next_level, game_over, bg_image)
+
+            # Обновление ракет и очков
             controls.update_rockets(stats, scs, ufos, rockets, hit)
+
+            # Обновление экрана
             controls.update(bg_image, screen, stats, scs, spaceship, ufos, rockets, lives)
+
             clock.tick(FPS)
-
-
-run()
