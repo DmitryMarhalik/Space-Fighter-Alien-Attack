978b0d630527d8a0d6e21e30ba81b07ead673c7d
DmitryMarhalik <dmitrymarhalik@gmail.com>
2024-01-14 11:58:45 +0300

    fix docstring

--- set_sound.py
+++ set_sound.py
@@ -5,7 +5,7 @@ def config_sound(file: str, volume: float = 0.4) -> pygame.mixer.Sound:
     """ Настраивает звуковой файл с определенной громкостью.
     Args:
         file (str): Путь к звуковому файлу.
-        volume (float): Уровень громкости звука.
+        volume (float): Уровень громкости звука по умолчанию.
     Return:
         pygame.mixer.Sound: настроенный звуковой объект.
     """
